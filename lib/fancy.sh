25f4f6c6d81fa2671560e3c47be3bfab052ab37c
Travis Kern <kern.travis@gmail.com>
2023-05-07 00:39:47 -0400

    Rewrite fancy.sh
    Redid stylized bash printing to use command line args.
    
    The args:
     * -s <style> to select a style
     * -c <color> to select a color
     * -n to disable newline
     * -h to print help
    
    This setup solved 3 problems with the previous version:
     - It simplified the script's argument processing. The previous version
       had to care about argument order. This version does not.
     - It made argument validation more straightforward
     - It removed the need for 2 functions. The previous version had one
       function for newline, one for no newline.

--- lib/fancy.sh
+++ lib/fancy.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Notes: 
-#     - Bash 4 is required for associative arrays.
+#     - Bash 4+ is required for associative arrays.
 # 
-FANCY_TEMPLATE="\e[STYLECOLORmVALUE\e[0m"
-
-declare -A FANCY_COLORS=( 
+declare -A __FANCY_COLORS=( 
     ["red"]=";31" 
     ["green"]=";32"
     ["yellow"]=";33"
@@ -13,45 +11,97 @@ declare -A FANCY_COLORS=(
     ["magenta"]=";35"
     ["cyan"]=";36"
     ["gray"]=";37"
-    ["none"]=""
 )
 
-declare -A FANCY_STYLES=(
+declare -A __FANCY_STYLES=(
     ["normal"]="0"
     ["bold"]="1"
     ["underline"]="4"
     ["reverse"]="7"
 )
 
-fancy_print() {
-    if [ "$#" -eq 3 ]; then
-        style="${FANCY_STYLES[$1]}"
-        color="${FANCY_COLORS[$2]}"
-        value=$3
-    elif [ "$#" -eq 2 ]; then
-        style="${FANCY_STYLES[$1]}"
-        color="${FANCY_COLORS[none]}"
-
-        if [ -z "$style" ]; then
-            style="${FANCY_STYLES[normal]}"
-            color="${FANCY_COLORS[$1]}"
-        fi
-
-        value=$2
-    elif [ "$#" -eq 1 ]; then
-        style="${FANCY_STYLES[normal]}"
-        color="${FANCY_COLORS[none]}"
-        value=$1
+function __validate_style {
+    if [ "${__FANCY_STYLES[$1]}" ]; then
+        return 0
+    else
+        fancy_print -s bold -c red "Invalid Style"
+        return 1
     fi
-
-    fancy="${FANCY_TEMPLATE//STYLE/$style}"
-    fancy="${fancy//COLOR/$color}"
-    fancy="${fancy//VALUE/$value}"
-
-    echo -en "$fancy"
 }
 
-fancy_println() {
-    fancy=$(fancy_print $@)
-    echo -e "$fancy"
+function __validate_color {
+    if [ "${__FANCY_COLORS[$1]}" ]; then
+        return 0
+    else
+        fancy_print -s bold -c red "Invalid Color"
+        return 1
+    fi
+}
+
+__fancy_usage() {
+    cmd_suffix="$1"
+    fancy_print -s bold -c cyan -n "Usage: "
+    echo "fancy_print [-s style] [-c color] [-n] [-h] value"
+    echo ""
+    fancy_print -s bold -c blue "Options: "
+    echo "    -s) Select style"
+    echo "    -c) Select color"
+    echo "    -n) Do not add newline to output"
+    echo "    -h) Print this help message"
+    echo ""
+    fancy_print -s bold -c green "Available Styles: "
+    for key in "${!__FANCY_STYLES[@]}"; do
+        echo "    $key"
+    done
+    echo ""
+    fancy_print -s bold -c green "Available Colors: "
+    for key in "${!__FANCY_COLORS[@]}"; do
+        echo "    $key"
+    done
+    echo ""
+}
+
+fancy_print() {
+    local help_suffix=""
+    local style=""
+    local color=""
+    local value=""
+    local newline="\n"
+
+    local OPTIND s c
+    while getopts "s:c:nh" opt; do
+        case $opt in
+            s)  
+                ! __validate_style "$OPTARG" && __fancy_usage && return 1
+                style="$OPTARG"
+                ;;
+            c)  
+                ! __validate_color "$OPTARG" && __fancy_usage && return 1
+                color="$OPTARG"
+                ;;
+            n)
+                newline=""
+                ;;
+            h)
+                __fancy_usage && return 0
+                ;;
+            *)  ;;
+        esac
+    done
+
+    shift $(($OPTIND - 1))
+
+    local value="$1"
+
+    if [[ "$value" == "" ]]; then
+        fancy_print -s bold -c red "Missing Value"
+        __fancy_usage
+        return 1
+    fi
+    [[ "$style" == "" ]] && style=0
+
+    local style_code="${__FANCY_STYLES[$style]}"
+    local color_code="${__FANCY_COLORS[$color]}"
+
+    printf "\e[${style_code}${color_code}m${value}\e[0m${newline}"
 }
