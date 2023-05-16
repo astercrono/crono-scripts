1d16c90b896ff5e109e4f66364fbf0b14d312870
Travis Kern <kern.travis@gmail.com>
2023-05-15 21:42:44 -0400

    Remove fail test from sample_test.sh suite

--- test/sample_test.sh
+++ test/sample_test.sh
@@ -1,30 +1,6 @@
 #!/usr/bin/env bash
 
-run_good_test() {
-    case_pass
-}
-
-run_bad_test() {
-    case_pass
-    case_pass
-    case_fail
-    case_pass
-}
-
 sample_foo_test() {
-    local testdata=(1 2 3)
-    for n in "${testdata[@]}"; do
-        case_set "Check $n"
-
-        if [ "$n" -eq "2" ]; then
-            case_fail "Failed because BAD. Very bad!"
-        else
-            case_pass
-        fi
-    done
-}
-
-sample_bar_test() {
     local testdata=(1 2)
     for n in "${testdata[@]}"; do
         case_set "Check $n"
@@ -33,4 +9,3 @@ sample_bar_test() {
 }
 
 run_test "sample_foo_test" "The Foo Test"
-run_test "sample_bar_test" "The Bar Test"
